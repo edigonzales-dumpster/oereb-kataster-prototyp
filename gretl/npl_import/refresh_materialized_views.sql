5dbf0bb9bb9d7209cc0f82639a813f9268a5c724
Stefan Ziegler <edi.gonzales@gmail.com>
2019-08-16 18:41:48 +0200

    wip

--- gretl/npl_import/refresh_materialized_views.sql
+++ gretl/npl_import/refresh_materialized_views.sql
@@ -1,3 +1,18 @@
 REFRESH MATERIALIZED VIEW 
-    live.v_nutzungsplanung_grundnutzung
+    live.vw_oerebwms_nutzungsplanung_grundnutzung_flaeche
+;
+REFRESH MATERIALIZED VIEW 
+    live.vw_oerebwms_nutzungsplanung_ueberlagernd_flaeche
+;
+REFRESH MATERIALIZED VIEW 
+    live.vw_oerebwms_nutzungsplanung_ueberlagernd_linie
+;
+REFRESH MATERIALIZED VIEW 
+    live.vw_oerebwms_nutzungsplanung_ueberlagernd_punkt
+;
+REFRESH MATERIALIZED VIEW 
+    live.vw_oerebwms_nutzungsplanung_sondernutzungsplaene_flaeche
+;
+REFRESH MATERIALIZED VIEW 
+    live.vw_oerebwms_nutzungsplanung_baulinien_linie
 ;
